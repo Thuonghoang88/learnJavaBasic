249225cab892b9e4b57dffa2189a1a67d3c9e346
Thuonghoang88 <36090621+Thuonghoang88@users.noreply.github.com>
2020-03-03 15:23:48 +0700

    Add files via upload

---
+++ PracticeJava.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practicejava;
+
+import java.util.List;
+
+/**
+ *
+ * @author admin
+ */
+public class PracticeJava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        /* // run lesson1
+        lesson1 ls1 = new lesson1();
+        List<Integer> lst = ls1.listNumber();
+        ls1.showListNumber(lst); */
+        /* // run lesson 2
+        lesson2 ls2 = new lesson2();
+        long n = ls2.dequygiaithua(5);
+        System.out.println(n);
+        long m = ls2.giaithua(5);
+        System.out.println(m); */
+        // run lesson 3
+       // lesson3 ls3 = new lesson3();
+       //ls3.showMap();
+       //run lesson 4
+      // lesson4 ls4 = new lesson4();
+      // ls4.showQuadraticEquation();
+      // run lesson 5
+      //lesson5 ls5 = new lesson5();
+      //ls5.showNumber();
+      // lesson 6
+      //lesson6 ls6 = new lesson6();
+      //ls6.showArrayFibonacci();
+      //lesson7 ls7 = new lesson7();
+      //ls7.show();
+      //lesson8 ls8 = new lesson8();
+      //ls8.showPrimeNumber();
+      //lesson9 ls9 = new lesson9();
+      //ls9.showPrimeNNumber();
+      //lesson10 ls10 = new lesson10();
+      //ls10.showNumberHasFiveNuber();
+      //lesson11 ls11 = new lesson11();
+      //ls11.showArray();
+      //lesson12 ls12 = new lesson12();
+      //ls12.show();
+     //lesson13 ls13 = new lesson13();
+     //ls13.showThuanNghich();
+     //lesson14 ls14 = new lesson14();
+     //ls14.show();
+     //lesson15 ls15 = new lesson15();
+     //ls15.Show();
+     //lesson16 ls16 = new lesson16();
+     //ls16.show();
+     //lesson16_02 ls1602 = new lesson16_02();
+     //ls1602.show();
+     //lesson17 ls17 = new lesson17();
+     //ls17.print();
+     
+    }
+    
+}
